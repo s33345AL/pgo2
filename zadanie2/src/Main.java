7100d9b6aeecca59895a3f8e0adafb6a955aaac9
Antoni Ledziński <s33345@pjwstk.edu.pl>
2025-03-13 13:42:16 +0100

    Do poprawy

--- zadanie2/src/Main.java
+++ zadanie2/src/Main.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double cenapodstawowa = 40;
+        System.out.println("Podaj wiek: ");
+        int wiek = sc.nextInt();
+        System.out.println("Czy mieszkasz w Warszawie: ");
+        String miejsce = sc.next();
+        System.out.println("Podaj dzień tygodnia: ");
+        String dzien = sc.next();
+        double znizka = 0;
+        double nowacena = cenapodstawowa - ((cenapodstawowa * znizka)/100);
 
-        System.out.println("Hello world!");
+
+        if (dzien == "czwartek" || dzien == "Czwartek") {
+            znizka = 100;
+
+
+            if (miejsce.equals("tak")) {
+                znizka = znizka + 10;
+
+                if (wiek < 10) {
+                    znizka = 100;
+                } else if (wiek >= 10 && wiek <= 18) {
+                    znizka = znizka + 50;
+
+                    if (znizka > 100) {
+                        znizka = 100;
+                    }
+
+                }
+            }
+        }
+        System.out.println(nowacena);
     }
 }
