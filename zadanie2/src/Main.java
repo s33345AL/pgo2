0dc59dd25ced904a113dbbe87a43ab88986898d8
Antoni <s33345@pjwstk.edu.pl>
2025-03-13 20:15:09 +0100

    Skończone zadanie

--- zadanie2/src/Main.java
+++ zadanie2/src/Main.java
@@ -3,36 +3,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double cenapodstawowa = 40;
+        int cenapodstawowa = 40;
+
         System.out.println("Podaj wiek: ");
         int wiek = sc.nextInt();
         System.out.println("Czy mieszkasz w Warszawie: ");
         String miejsce = sc.next();
         System.out.println("Podaj dzień tygodnia: ");
         String dzien = sc.next();
-        double znizka = 0;
-        double nowacena = cenapodstawowa - ((cenapodstawowa * znizka)/100);
+        int znizka = 0;
+        int cenakoncowa = cenapodstawowa;
 
-
-        if (dzien == "czwartek" || dzien == "Czwartek") {
+        if (wiek < 10) {
             znizka = 100;
-
-
-            if (miejsce.equals("tak")) {
-                znizka = znizka + 10;
-
-                if (wiek < 10) {
-                    znizka = 100;
-                } else if (wiek >= 10 && wiek <= 18) {
+            cenakoncowa = 0;
+        } else {
+            if (wiek >= 10 && wiek <= 18) {
+                znizka = 50;
+                cenakoncowa = 20;
+            }
+            if (miejsce.equals("tak")){
+                znizka = 10;
+                cenakoncowa = cenakoncowa - (cenapodstawowa * znizka)/100;
+                if (wiek >= 10 && wiek <= 18 && miejsce.equals("tak")) {
                     znizka = znizka + 50;
-
-                    if (znizka > 100) {
-                        znizka = 100;
-                    }
-
                 }
             }
+            if (dzien.equals("czwartek")  || dzien.equals("Czwartek")) {
+                znizka = 100;
+                cenakoncowa = 0;
+            }
         }
-        System.out.println(nowacena);
+        System.out.println(cenakoncowa + " " + znizka);
     }
 }
